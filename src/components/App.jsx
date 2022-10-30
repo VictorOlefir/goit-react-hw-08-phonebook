3f79744fd68e94f7b3c610be9925026e05ba12d6
VictorOlefir <99094476+VictorOlefir@users.noreply.github.com>
2022-10-30 15:08:00 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,64 @@
-import {useState, useEffect} from "react";
-import { nanoid } from 'nanoid'
-import { Box } from "./Box";
-import { Form } from "./ContactForm/ContactForm";
-import { ContactList } from "./ContactList/ContactList";
-import { Filter } from "./Filter/Filter";
-
-const LS_KEY = 'contacts';
-const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { Box } from './Box';
+import { Form } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { LS_KEY, initialContacts } from './constants/constants';
 
 export const App = () => {
-    const [contacts, setContacts] = useState(()=> JSON.parse(localStorage.getItem(LS_KEY)) ?? initialContacts);
-    const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem(LS_KEY)) ?? initialContacts
+  );
+  const [filter, setFilter] = useState('');
 
-    useEffect(() => {
-      localStorage.setItem(LS_KEY, JSON.stringify(contacts))
-      
-    }, [contacts]);
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
-  const addContact = (data) => {
-         
-    const duplicateContact =
-      contacts.find(contact => contact.name.toLowerCase() === data.name.toLowerCase());
-    
-    if (duplicateContact) return window.alert(`${data.name} is already in contacts`);
+  const addContact = data => {
+    const duplicateContact = contacts.find(
+      contact => contact.name.toLowerCase() === data.name.toLowerCase()
+    );
+
+    if (duplicateContact)
+      return window.alert(`${data.name} is already in contacts`);
     const contact = { ...data, id: nanoid() };
     setContacts(prev => [...prev, contact]);
   };
 
-  const getVisibleContact = () => {    
+  const getVisibleContact = () => {
     const normalizeFilter = filter.toLowerCase();
-    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizeFilter));
-  }
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
 
-  const deleteContacts = (contactId) => {
-    setContacts(prev => prev.filter(contact => contact.id !== contactId) )
-    
-  }
+  const deleteContacts = contactId => {
+    setContacts(prev => prev.filter(contact => contact.id !== contactId));
+  };
 
-   const visibleContact = getVisibleContact();
-
+  const visibleContact = getVisibleContact();
 
   return (
-      <Box width="30%" mt={3} mb={3} ml={6}
-        p={ 4} bg='white' borderRadius='normal'
-        boxShadow='card'>
-        <h1>Phonebook</h1>
-        <Form onSubmit={addContact} />
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={(event) => setFilter(event.target.value)} />      
-        <ContactList contacts={visibleContact} onDeleteContact={deleteContacts } />
-      </Box>
-    
-    )
-  
-}
+    <Box
+      width="30%"
+      mt={3}
+      mb={3}
+      ml={6}
+      p={4}
+      bg="white"
+      borderRadius="normal"
+      boxShadow="card"
+    >
+      <h1>Phonebook</h1>
+      <Form onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter
+        value={filter}
+        onChange={event => setFilter(event.target.value)}
+      />
+      <ContactList contacts={visibleContact} onDeleteContact={deleteContacts} />
+    </Box>
+  );
+};
